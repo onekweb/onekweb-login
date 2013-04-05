7a8a10e418459ca1e8dd99d50d997b5a5eac436c
Mario Onekanda <mario.onekanda@gmail.com>
2013-04-05 17:37:52 +0200

    my first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>Onekweb.com</title>
+</head>
+<body>
+   <div id="content"> 
+    
+        <form id ="login" action=""  method="post">
+            <h1>Log in</h1>
+            <fieldset id="inputs">
+                <p><input id="userame"type="name" value"" placeholder="Username"</p>
+                <p><input id="password"type="password" value="" placeholder="Password"</p>        
+            </fieldset>
+            
+            <fieldset id="actions">
+                <input type="submit" id="submit" value="Log in"><br />
+            <a href="">Register</a>
+            </fieldset>
+        </form>
+   </div>
+        
+        
+</body>
+</html>
